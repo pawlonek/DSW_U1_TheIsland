31c2df159dd504607609de1c2465427115a9f386
olej500 <olej500@gmail.com>
2025-05-29 11:33:44 +0200

    finished portal and sign

---
+++ Assets/Olej/Portal/Portal.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject puzzle;
+    private PuzzleInput puzzleScript;
+    private AudioSource woosh;
+    
+    private BoxCollider box;
+    private MeshCollider mesh;
+
+    private GameObject effect;
+    public float vanishSpeed = 1f;
+    private Vector3 vanishStartSize;
+    private Vector3 vanishStep;
+
+    private bool success;
+    private bool vanish;
+    private bool set;
+
+    void Start()
+    {
+        box = GetComponent<BoxCollider>();
+        mesh = GetComponent<MeshCollider>();
+        woosh = GetComponent<AudioSource>();
+        puzzleScript = puzzle.GetComponent<PuzzleInput>();
+        effect = transform.Find("PortalEffect").gameObject;
+        vanishStartSize = effect.transform.localScale;
+        vanishStep = vanishStartSize * vanishSpeed / 100;
+    }
+
+    void Update()
+    {
+        if (puzzleScript.success && !set)
+        {
+            success = true;
+            vanish = true;
+            set = true;
+        }
+
+        if(success)
+        {
+            Success();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (vanish)
+        {
+            effect.transform.localScale -= vanishStep;
+            if (effect.transform.localScale.x < 0)
+            {
+                Destroy(effect);
+                vanish = false;
+            }
+        }
+    }
+
+    void Success()
+    {
+        box.enabled = false;
+        woosh.Play();
+        success = false;
+    }
+
+}
