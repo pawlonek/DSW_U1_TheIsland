5e28d7deec3e80649faf62a643603687cd51f9d2
olej500 <olej500@gmail.com>
2025-05-29 12:59:20 +0200

    game manager added
    added game manager with the turning off of the volcano barrier collider

---
+++ Assets/Olej/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject terrain;
+    public GameObject finalPuzzle;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (finalPuzzle.GetComponent<PuzzleInput>().success)
+        {
+            terrain.GetComponent<CapsuleCollider>().enabled = false;
+        }
+    }
+}
